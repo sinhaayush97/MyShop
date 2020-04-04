0de8192dd345bde10b64b62daa8c74e478bf0833
sinhaayush97 <62948391+sinhaayush97@users.noreply.github.com>
2020-04-04 18:28:56 +0530

    Image Uploading for products is done.

--- MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductManagerController.cs
@@ -7,6 +7,7 @@ using MyShop.Core.Models;
 using MyShop.Core.ViewModels;
 using MyShop.DataAccess.InMemory;
 using MyShop.Core.Contracts;
+using System.IO;
 
 namespace MyShop.WebUI.Controllers
 {
@@ -38,7 +39,7 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product,HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -46,6 +47,11 @@ namespace MyShop.WebUI.Controllers
             }
             else
             {
+                if(file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath(@"/Content/ProductImages/" + product.Image));
+                }
                 context.Create(product);
                 context.Commit();
                 return RedirectToAction("Index");
@@ -69,7 +75,7 @@ namespace MyShop.WebUI.Controllers
         }
 
         [HttpPost]
-        public ActionResult Edit(Product product,string Id)
+        public ActionResult Edit(Product product,string Id,HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if(productToEdit == null)
@@ -82,9 +88,13 @@ namespace MyShop.WebUI.Controllers
                 {
                     return View(product);
                 }
+                if(file != null)
+                {
+                    productToEdit.Image = productToEdit.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath(@"/Content/ProductImages/" + productToEdit.Image));                
+                }
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
                 context.Commit();
