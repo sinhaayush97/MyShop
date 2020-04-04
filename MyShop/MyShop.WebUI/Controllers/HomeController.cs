8868378f8aa1285cf4546743d14fe6374c8cb662
sinhaayush97 <62948391+sinhaayush97@users.noreply.github.com>
2020-04-04 23:02:41 +0530

    Product List Filter created

--- MyShop/MyShop.WebUI/Controllers/HomeController.cs
+++ MyShop/MyShop.WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using MyShop.Core.Contracts;
 using MyShop.Core.Models;
+using MyShop.Core.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -18,16 +19,35 @@ namespace MyShop.WebUI.Controllers
             this.productContext = productContext;
             this.productCategoriesContext = productCategoriesContext;
         }
-        public ActionResult Index()
+        public ActionResult Index(string Category=null)
         {
-            List<Product> productList = productContext.Collection().ToList();
-            return View(productList);        
+            List<Product> productList;
+            List<ProductCategory> productCategoryList = productCategoriesContext.Collection().ToList();
+            if(Category == null)
+            {
+                productList = productContext.Collection().ToList();
+            }
+            else
+            {
+                productList = productContext.Collection().Where(item => item.Category == Category).ToList();
+            }
+            ProductListViewModel objProductListViewModel = new ProductListViewModel();
+            objProductListViewModel.Product = productList;
+            objProductListViewModel.ProductCategories = productCategoriesContext.Collection().ToList();
+            return View(objProductListViewModel);        
         }
 
         public ActionResult Details(string ID)
         {
             Product product = productContext.Find(ID);
+            if(product == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
             return View(product);
+            }
         }
 
         public ActionResult About()
