5fca8205474928ff838acb45fa7e451db9484a31
sinhaayush97 <62948391+sinhaayush97@users.noreply.github.com>
2020-04-07 13:27:19 +0530

    Added Customer Registration

--- MyShop/MyShop.WebUI/Controllers/AccountController.cs
+++ MyShop/MyShop.WebUI/Controllers/AccountController.cs
@@ -8,6 +8,8 @@ using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Microsoft.Owin.Security;
+using MyShop.Core.Contracts;
+using MyShop.Core.Models;
 using MyShop.WebUI.Models;
 
 namespace MyShop.WebUI.Controllers
@@ -17,15 +19,21 @@ namespace MyShop.WebUI.Controllers
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
+        IRepository<Customer> customerRepository;
 
-        public AccountController()
-        {
-        }
+        //AS: Commented. As there is only one constructor required for injecting IRepository
+        //-- Unity injects through constructor to controller automitacally.
+        //public AccountController()
+        //{
+        //}
 
-        public AccountController(ApplicationUserManager userManager, ApplicationSignInManager signInManager )
+        public AccountController( IRepository<Customer> customerRepository)
         {
-            UserManager = userManager;
-            SignInManager = signInManager;
+            //ApplicationUserManager userManager, ApplicationSignInManager signInManager -> Constructor Paraameter.
+            //UserManager = userManager;
+            //SignInManager = signInManager;
+
+            this.customerRepository = customerRepository;
         }
 
         public ApplicationSignInManager SignInManager
@@ -155,6 +163,19 @@ namespace MyShop.WebUI.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
+                    //Register the custom model.
+                    Customer customer = new Customer {
+                        FirstName = model.FirstName,
+                        LastName = model.LastName,
+                        Email = model.Email,
+                        Street = model.Street,
+                        City = model.City,
+                        State = model.State,
+                        ZipCode = model.ZipCode,
+                        UserID = user.Id
+                    };
+                    customerRepository.Create(customer);
+                    customerRepository.Commit();
                     await SignInManager.SignInAsync(user, isPersistent:false, rememberBrowser:false);
                     
                     // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
